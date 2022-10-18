9174ab4ab8f14a6ff9b0076d4d3d0a67c90470f0
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2022-10-18 07:25:18 -0400

    ajustes

--- .scriptInit/init.sql
+++ .scriptInit/init.sql
@@ -1,7 +1,7 @@
-GRANT ALL PRIVILEGES ON * . * TO 'candidato'@'%';
+-- ALTER TABLE `desafio-dev-back-1`.colaborador MODIFY COLUMN data_nascimento datetime(6) NULL;
 
-INSERT INTO `desafio-dev-back-1`.setor (nome,id)
-	VALUES ('RH',1);
-INSERT INTO `desafio-dev-back-1`.setor (nome,id)
-	VALUES ('DP',2);
+-- INSERT INTO `desafio-dev-back-1`.setor (nome,id)
+-- 	VALUES ('RH',1);
+-- INSERT INTO `desafio-dev-back-1`.setor (nome,id)
+-- 	VALUES ('DP',2);
 
